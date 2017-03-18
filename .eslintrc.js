3e5aae5b22c53acd06e0a1795bbd40df2a59e8eb
Justin Morris <desk@pixelbloom.com>
2017-03-19 10:35:37 +1100

    Very minor tooling/packaging cleanups.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    'extends': ['airbnb-base'],
-    'rules': {
-        'import/no-extraneous-dependencies': 'off',
-        'import/no-webpack-loader-syntax': 'off',
-        'import/no-unresolved': 'off',
-        'import/extensions': 'off',
-    },
-    'env': {
-        'mocha': true
-    },
+  extends: ['airbnb-base'],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-webpack-loader-syntax': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+  },
+  env: {
+     mocha: true
+  },
 };
